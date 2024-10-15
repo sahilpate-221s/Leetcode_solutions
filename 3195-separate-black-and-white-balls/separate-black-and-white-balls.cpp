cec7959bbd072ec01c362bc69b19d935b46f2cf9
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2024-10-15 13:29:28 +0530

    Time: 36 ms (32.00%) | Memory: 15.6 MB (76.42%) - LeetSync

---
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+    long long ans = 0;
+    long long white = 0;
+    for(int i=0;i<s.length();i++)
+    {
+        if(s[i]=='0')
+        {
+            ans+=white;
+        }
+        else
+        {
+            white++;
+        }
+    }
+    return ans;
+
+        
+    }
+};
