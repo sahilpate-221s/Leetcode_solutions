fc525d1728e8a42efc215658c1f042f769dddd77
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2024-10-20 23:02:48 +0530

    Time: 74 ms (79.88%) | Memory: 62.6 MB (6.47%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,60 +1,40 @@
 class Solution {
 public:
-    vector<int>parent;
-    vector<int> rank;
-    int find(int x)
+    void dfs(unordered_map<int,vector<int>>& adj, int u, vector<bool>& visited)
     {
-        if(parent[x] == x)
+        
+        visited[u]=true;
+        for(int &v : adj[u])
         {
-            return x;
+            if(!visited[v])
+            {
+                dfs(adj,v,visited);
+            }
+        }
+    }
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        
+        if(connections.size() < n-1)return -1;
+      // Build the adjacency list from the connections
+        unordered_map<int, vector<int>> adj;
+        for (auto &conn : connections)
+        {
+            int u = conn[0];
+            int v = conn[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
         
-        return find(parent[x]);
-    }
-
-    void Union(int x,int y)
-    {
-        int x_parent = find(x);
-        int y_parent = find(y);
-
-        if(rank[x_parent] > rank[y_parent])
-        {
-            parent[y_parent] = x_parent;
-        }
-        else if(rank[x_parent] < rank[y_parent])
-        {
-            parent[x_parent] = y_parent;
-        }
-        else
-        {
-            parent[x_parent] = y_parent;
-            rank[y_parent]++;
-        }
-    }
-
-
-    int makeConnected(int n, vector<vector<int>>& connections) {
-        parent.resize(n);
-        rank.resize(n,0);
-
-        if(connections.size() < n-1)return -1;
-
+        vector<bool> visited(n,0);
+        int count=0;
         for(int i=0;i<n;i++)
         {
-            parent[i]=i;
-        }
-        int components = n;
-
-        for(auto& vec: connections)
-        {
-            if(find(vec[0]) != find(vec[1]))
+            if(!visited[i])
             {
-                Union(vec[0],vec[1]);
-                components--;
+                dfs(adj,i,visited);
+                count++;
             }
-
         }
-        return components-1;
-        
+        return count-1;
     }
 };
