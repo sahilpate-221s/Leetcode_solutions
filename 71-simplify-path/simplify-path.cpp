ca1320366fcff64017330f98197809be8fdb93da
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2024-10-22 18:16:26 +0530

    Time: 7 ms (36.01%) | Memory: 11.8 MB (30.83%) - LeetSync

---
+++ 71-simplify-path/simplify-path.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> st;
+        stringstream ss(path);
+        string token = "";
+
+        while(getline(ss,token,'/'))
+        {
+            if(token=="" || token== ".")continue;
+            
+            if(token != "..")
+            {
+                st.push(token);
+            }
+            else if(!st.empty())
+            {
+                st.pop();
+            }
+        }
+
+        if(st.empty())
+        {
+            return "/";
+        }
+        string result = "";
+
+        while(!st.empty())
+        {
+            result= "/"+st.top()+result;
+            st.pop();
+        }
+        return result;
+        
+    }
+};
