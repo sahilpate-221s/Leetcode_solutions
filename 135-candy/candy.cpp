b157c624bb30ec0e5ba15ab16184939ff8ce2bae
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2024-11-22 11:47:19 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (57.40%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -3,15 +3,15 @@ public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
         
-        vector<int> L2R(n,1);
-        vector<int> R2L(n,1);
+
+        vector<int> count(n,1);
 
         //left to right candy check
         for(int i=1;i<n;i++)
         {
             if(ratings[i] > ratings[i-1])
             {
-                L2R[i] = max(L2R[i], L2R[i-1]+1);
+                count[i] = max(count[i], count[i-1]+1);
             }
         }
 
@@ -19,14 +19,14 @@ public:
         {
             if(ratings[i] > ratings[i+1])
             {
-                R2L[i] = max(R2L[i], R2L[i+1]+1);
+                count[i] = max(count[i], count[i+1]+1);
             }
         }
 
         int result = 0;
         for(int i=0;i<n;i++)
         {
-            result += max(L2R[i],R2L[i]);
+            result += count[i];
             
         }
         return result;
