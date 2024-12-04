738f9c7567cb2a2eca573a81bee6db595dafd4c4
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2024-12-04 15:25:08 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (39.64%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,46 +1,26 @@
 class Solution {
 public:
-    int helper(int index, vector<int>&nums,int prev,vector<vector<int>>& dp)
-    {
-        if(index == nums.size())
-        {
-            return 0;
-        }
-        if(prev != -1 &&  dp[index][prev] != -1)return  dp[index][prev];
-
-        int   noTake = helper(index+1,nums,prev,dp);
-        int take = 0;
-        if(prev == -1 || nums[index] > nums[prev])
-        {
-            take = 1 + helper(index+1,nums,index,dp);
-        }
-        if(prev != -1)
-            dp[index][prev] = max(take,noTake);
-
-        return max(take,noTake);
-        
-    }
     int lengthOfLIS(vector<int>& nums) {
+
+        //patience sorting
         int n = nums.size();
-        // vector<vector<int>>dp(n,vector<int>(n,1));
-        // return helper(0,nums,-1,dp);
+        vector<int>sorted;
 
-        vector<int>dp(n,1);
-
-        //tabulation code 
-
-        int maxLIS = 1;
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<i;j++)
+            auto it = lower_bound(begin(sorted),end(sorted),nums[i]); // justgreater than or equal to nums[i];
+
+            if(it == end(sorted))
             {
-                if(nums[j] < nums[i])
-                {
-                    dp[i] = max(dp[i],dp[j]+1);
-                }
-                maxLIS = max(maxLIS,dp[i]);
+                sorted.push_back(nums[i]);
+            }
+            else
+            {
+                *it = nums[i];
             }
         }
-        return  maxLIS;
+        return sorted.size();
+        
+        
     }
 };
