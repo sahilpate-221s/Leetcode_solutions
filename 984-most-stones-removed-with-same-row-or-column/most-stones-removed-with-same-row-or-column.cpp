88c7496bb4b92219ed7f74762d06c589cc041e2a
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2024-12-15 17:12:18 +0530

    Time: 78 ms (23.63%) | Memory: 17.9 MB (94.35%) - LeetSync

---
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+int n ;
+    void dfs(vector<vector<int>>& stones,int index, vector<bool>& visited)
+    {
+        visited[index]= true;
+
+        for(int i=0;i<n;i++)
+        {
+            if(!visited[i] && ((stones[i][0] == stones[index][0] || stones[i][1] == stones[index][1])))
+            {
+                dfs(stones,i,visited);
+            }
+        }
+    }
+    int removeStones(vector<vector<int>>& stones) {
+        n = stones.size();
+        vector<bool> visited(n,false);
+
+        int groups = 0;
+
+        for(int i=0;i<n;i++)
+        {
+            if(visited[i])continue;
+
+            dfs(stones,i,visited);
+            groups++;
+        }
+
+        return (n-groups);
+        
+    }
+};
