d40370c24c281ffa53e146863c41327501303a4e
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-01-01 10:53:27 +0530

    Time: 3 ms (18.71%) | Memory: 9.6 MB (6.19%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -2,28 +2,31 @@ class Solution {
 public:
     int maxScore(string s) {
         int n = s.length();
+        vector<int> zero_prefix(n+1,0);
+        vector<int> one_suffix(n+1,0);
 
-        int count0 =0;
-        int count1=0;
-        for(auto &it:s)
-        {
-            if(it=='1')count1++;
-        }
 
         int maxCount = INT_MIN;
-        for(int i=0;i<n-1;i++)
+        //prefix sum for zero count
+        for(int i=1;i<=n;i++)
         {
-            if(s[i]=='0')
-            {
-                count0++;
-            }
-            else
-            {
-                count1--;
-            }
+            zero_prefix[i] = zero_prefix[i-1]+ (s[i-1]=='0'?1:0);
+        }
 
-            maxCount = max(maxCount, count0+count1);
+        //suffix sum for one count 
+        for(int i=n-1;i>=0;i--)
+        {
+            one_suffix[i] = one_suffix[i+1] + (s[i]=='1'?1:0);
+        }
+
+        // now traverse from left to rigth ans add the zero and one 
+        for(int i=1;i<n;i++)
+        {
+            maxCount  = max(maxCount, one_suffix[i]+zero_prefix[i]);
         }
         return maxCount;
+
+
+        
     }
 };
