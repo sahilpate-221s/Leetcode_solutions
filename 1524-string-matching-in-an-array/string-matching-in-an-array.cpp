3e4998e15d39ec70959ad5ee38fc9f34e630913b
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-01-07 11:26:44 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (60.52%) - LeetSync

---
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+        vector<string> answer;
+        int n = words.size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==j)continue;
+
+                if(words[j].find(words[i]) != string::npos)
+                {
+                    answer.push_back(words[i]);
+                    break;
+                }
+            }
+        }
+
+        return answer;
+        
+    }
+};
