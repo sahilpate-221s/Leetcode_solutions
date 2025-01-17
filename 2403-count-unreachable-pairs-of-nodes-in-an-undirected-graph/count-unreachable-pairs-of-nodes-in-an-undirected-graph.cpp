8b584d6566b3bff6e944b862fa141b1b11d79766
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-01-17 12:49:47 +0530

    Time: 192 ms (22.71%) | Memory: 205.9 MB (20.55%) - LeetSync

--- 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,70 +1,37 @@
 class Solution {
 public:
-    vector<int>parent;
-    vector<int>rank;
-   
-    int find(int x)
-    {
-        if(parent[x]==x)
-        {
-            return x;
-        }
-        return parent[x] = find(parent[x]);
-    }
-
-     void Union(int x, int y)
-    {
-        int x_parent = find(x);
-        int y_parent = find(y);
-
-        if(rank[x_parent] > rank[y_parent])
-        {
-            parent[y_parent] = x_parent;
-        }
-        else if(rank[x_parent] < rank[y_parent])
-        {
-            parent[x_parent] = y_parent;
-        }
-        else
-        {
-            parent[x_parent] = y_parent;
-            rank[y_parent]++;
+    void dfs(int u, vector<bool>& visited, unordered_map<int, vector<int>>& adj,
+             long long& size) {
+        visited[u] = true;
+        size++;
+        for (auto& v : adj[u]) {
+            if (!visited[v]) {
+                dfs(v, visited, adj, size);
+            }
         }
     }
-
-
     long long countPairs(int n, vector<vector<int>>& edges) {
-        parent.resize(n);
-        rank.resize(n,0);
+        long long result = 0;
+        long long currentSum = n;
 
-        for(int i=0;i<n;i++)
-        {
-            parent[i] = i;
-        }
-        //step2 ->making components
-        for(auto &vec: edges)
-        {
+        unordered_map<int, vector<int>> adj;
+        vector<bool> visited(n, false);
+
+        for (auto& vec : edges) {
             int u = vec[0];
             int v = vec[1];
-            Union(u,v);
 
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
 
-        // step3// map bana do for storing parent/components and number of nodes inside the node
-         unordered_map<int,int>umap;
-        for(int i=0;i<n;i++)
-        {
-            int Parent = find(i);
-            umap[Parent]++;
-        }
-        // find result from map by iteratingl
-        long long result = 0;
-        long long rem = n;
-        for(auto &it:umap)
-        {
-            long long size = it.second;
-            result+= (size)*(rem-size);
-            rem-=size;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                long long size = 0;
+                dfs(i, visited, adj, size);
+                result += (size) * (currentSum - size);
+                currentSum -= size;
+            }
         }
 
         return result;
