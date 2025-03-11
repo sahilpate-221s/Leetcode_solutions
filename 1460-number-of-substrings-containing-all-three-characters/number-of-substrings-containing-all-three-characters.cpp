fc5c36147036f7fc1e40cb7bc24b31276e0876e1
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-03-11 10:52:30 +0530

    Time: 35 ms (17.57%) | Memory: 11.3 MB (22.60%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int n= s.length();
+        int n = s.length();
 
-        unordered_map<int,int>umap;
+        unordered_map<int, int> umap;
 
         int count = 0;
         int j = 0;
 
-        for(int i=0;i<n;i++)
-        {
-            umap[s[i]-'a']++;
+        for (int i = 0; i < n; i++) {
+            umap[s[i] - 'a']++;
 
-            while(umap[0] > 0 && umap[1] > 0 && umap[2] > 0) 
-            {
-                count+= n-i; // iska matlab hai iss element tak mil gye to iske baad ke sare range me honge thats why we do n-i
-                ;
-                umap[s[j]-'a']--;
+            while (umap[0] > 0 && umap[1] > 0 && umap[2] > 0) {
+                count +=
+                    n - i; // iska matlab hai iss element tak mil gye to iske
+                           // baad ke sare range me honge thats why we do n-i
+                umap[s[j] - 'a']--;
                 j++;
             }
         }
