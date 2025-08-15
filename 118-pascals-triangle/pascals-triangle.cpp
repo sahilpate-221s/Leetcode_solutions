e0bfea5461837b7a6b1dcbf81596da41a405da92
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-08-15 22:39:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (32.28%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -3,21 +3,16 @@ public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>>answer(numRows);
 
-
         for(int i=0;i<numRows;i++)
         {
-            // har ith index ke liye 1 vector chahiye
             answer[i] = vector<int>(i+1,1);
 
-            // ek j loop lagega jo colums traverse karegas
             for(int j=1;j<i;j++)
             {
                 answer[i][j] = answer[i-1][j] + answer[i-1][j-1];
             }
         }
-
         return answer;
-
         
     }
 };
