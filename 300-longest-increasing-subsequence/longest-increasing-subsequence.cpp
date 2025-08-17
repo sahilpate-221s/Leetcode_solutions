0e4b30db8fd431510e3da962d2d9dc9e5ea0357b
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-08-17 15:42:51 +0530

    Time: 551 ms (20.88%) | Memory: 296.7 MB (6.87%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-
-        //patience sorting
-        int n = nums.size();
-        vector<int>sorted;
-
-        for(int i=0;i<n;i++)
-        {
-            auto it = lower_bound(begin(sorted),end(sorted),nums[i]); // justgreater than or equal to nums[i];
-
-            if(it == end(sorted))
-            {
-                sorted.push_back(nums[i]);
-            }
-            else
-            {
-                *it = nums[i];
-            }
-        }
-        return sorted.size();
+    int helper(int idx, vector<int>&nums, int prev, vector<vector<int>>& dp)
+    {
+        //base case
+        if(idx == nums.size())return 0;
         
+        if(dp[idx][prev+1] != -1)return dp[idx][prev+1];
+
+        int noTake = helper(idx+1,nums,prev,dp);
+        int take = 0;
+        if(prev == -1 || nums[idx] > nums[prev])
+        {
+            take = 1 + helper(idx+1,nums,idx,dp);
+        }
+
+        return dp[idx][prev+1] = max(noTake, take);
+
+
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+
+        return helper(0,nums,-1,dp);
         
     }
 };
