f66a99f659ede3aa711efd164289a047c9945d5b
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-09-05 05:13:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (23.85%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,57 +1,45 @@
 class Solution {
 public:
-    bool  isPossible(int n,vector<int>& nums,int k, int mid)
-    {
-        int subarraySize = 1;
-        int subarrayValue = 0;
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i] >mid)
-            {
+    bool isPossible(vector<int>& nums, int mid, int k) {
+        int subarry = 1;
+        int sum = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > mid)
                 return false;
+
+            if (sum + nums[i] > mid) {
+                subarry++;
+                sum = nums[i];
+                // mid represents our current guess for the maximum allowed sum.
+                // If splitting requires more than k subarrays, then:
+                // mid is too small because we cannot fit all elements into only k subarrays without exceeding mid.
+                // So, this configuration is not possible.
+
+                if(subarry > k)return false;
+            } else {
+                sum += nums[i];
             }
-
-            if(subarrayValue + nums[i] > mid)
-            {
-                subarraySize++;
-                subarrayValue = nums[i];
-
-                if(subarraySize > k)return false;
-            }
-            else
-            {
-                subarrayValue+=nums[i];
-            }
-
-
         }
         return true;
     }
     int splitArray(vector<int>& nums, int k) {
-        int n = nums.size();
 
-        if(k>n)return -1;
-        int sum = accumulate(nums.begin(),nums.end(),0);
         int left = 0;
-        int right = sum;
+        int right = accumulate(begin(nums), end(nums), 0);
         int ans = -1;
+        while (left <= right) {
 
-        while(left <= right)
-        {
-            int mid = left+(right-left)/2;
+            long long mid = (left + right) / 2;
 
-            if(isPossible(n,nums,k,mid))
-            {
-                ans=mid;
-                right = mid-1;
-            }
-            else
-            {
-                left = mid+1;
+            if (isPossible(nums, mid, k)) {
+                ans = mid;
+                right = mid - 1;
+
+            } else {
+                left = mid + 1;
             }
         }
-
         return ans;
-        
     }
 };
