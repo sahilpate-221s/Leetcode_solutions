14c398a139dd0db6a12e3ac9a226e6610229b077
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-09-09 05:11:16 +0530

    Time: 21 ms (67.55%) | Memory: 139.1 MB (59.79%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        
+        int n = nums.size();
         deque<int>dq;
         vector<int>answer;
 
-        for(int i=0;i<nums.size();i++)
+        for(int i=0;i<n;i++)
         {
-            //first case check for window size
             if(!dq.empty() && dq.front() == i-k)
             {
                 dq.pop_front();
             }
 
-            //second case remove all the elemtns which are smaller that the current
+            // second case remove all the elem jo smaller hai current se
             while(!dq.empty() && nums[i] > nums[dq.back()])
             {
                 dq.pop_back();
             }
 
-            // third case
+            // current index ko push kar do
             dq.push_back(i);
 
-            // forth case if we get widow size push max element
+            // jab window size mil jaye tab push kar do
             if(i>=k-1)
             {
                 answer.push_back(nums[dq.front()]);
             }
-            
         }
         return answer;
-        
     }
 };
