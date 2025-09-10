02a584251c488e7701057bb918223e73e206fcf8
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-09-10 17:24:08 +0530

    Time: 3 ms (14.17%) | Memory: 10.4 MB (5.36%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,49 +1,54 @@
 class Solution {
 public:
-    int helper(string& s, int i,int result,int sign)
+    int helper(string s, int i, int result , int sign )
     {
-        if(i>=s.length() || !isdigit(s[i]))return result;
+        if(i>=s.length() || !isdigit(s[i]))
+        {
+            return result;
+        }
 
         int digit = s[i]-'0';
 
-        //overflow handle karna 
+        // overflow se bachne ke liye 
         if(result > (INT_MAX - digit)/10)
         {
-            return sign == 1 ? INT_MAX : INT_MIN;
+            return sign == 1 ? INT_MAX: INT_MIN;
         }
-
         result  = result * 10 + digit;
-        return helper(s,i+1,result,sign);
+        return helper(s, i+1,result, sign);
     }
     int myAtoi(string s) {
+        
         int n = s.length();
-
+        int i = 0;
         int sign = 1;
 
-        int i=0;
-        
-        //checking for whiespaces
-        while(i<n && s[i]==' ')
+        //step1 removing the leading whitespaces
+        while(i<n)
         {
+            if(s[i] == ' ')
+            {
+                i++;
+            }
+            else break;
+        }
+
+        // steps 2 : checking for negative signs 
+        if(s[i] == '-' || s[i] == '+')
+        {
+            if(s[i] == '-')sign = -1;
             i++;
         }
 
-        // now checking for sign 
-        if(s[i] == '-' || s[i]=='+')
-        {
-            if(s[i]=='-') sign = -1;
-            i++;
-        }
+        // step 3 converting the remainign valid string to the number;
+        int result = helper(s, i, 0, sign);
 
-        int result = helper(s, i,0,sign);
-
-        if(sign == -1 && result == INT_MIN)return INT_MIN;
-        if(sign == -1 )return -result;
-
+        // if number is negative and not valid so return the MIN value possible
+        if(sign==-1 && result == INT_MIN)return INT_MIN;
+        // if number is negative and is valid then we return the negative number
+        if(sign == -1)return -result;
+        // else return the original number which we get
         return result;
-        
 
-
-        
     }
 };
