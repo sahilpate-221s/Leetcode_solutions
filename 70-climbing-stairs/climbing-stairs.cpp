0cd7ad3f565a1da3785aa5363e8a07655946ead2
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-09-12 04:49:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (37.04%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -14,19 +14,27 @@ public:
     int climbStairs(int n) {
         // return solve(n);
 
-        if (n == 0)
-            return 1;
-        if (n == 1)
-            return 1;
+        // if (n == 0)
+        //     return 1;
+        // if (n == 1)
+        //     return 1;
 
-        int prev1 = 1;
-        int prev2 = 1;
+        // int prev1 = 1;
+        // int prev2 = 1;
 
-        for (int i = 2; i <= n; i++) {
-            int val = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = val;
+        // for (int i = 2; i <= n; i++) {
+        //     int val = prev1 + prev2;
+        //     prev2 = prev1;
+        //     prev1 = val;
+        // }
+
+        vector<int>dp(n+1,1);
+
+        for(int i=2;i<=n;i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        return prev1;
+        return dp[n];
+        // return prev1;
     }
 };
