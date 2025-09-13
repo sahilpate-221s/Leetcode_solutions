1180dfe62000ad276fcbc46a702a8251d6965783
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-09-14 02:41:16 +0530

    Time: 564 ms (15.91%) | Memory: 296.5 MB (13.46%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    int helper(int idx, vector<int>&nums, int prev, vector<vector<int>>& dp)
-    {
-        //base case
-        if(idx == nums.size())return 0;
-        
-        if(dp[idx][prev+1] != -1)return dp[idx][prev+1];
+    int solve(int idx, int prev, vector<int>& nums, vector<vector<int>>& dp) {
+        // base case
+        if (idx == nums.size()) {
 
-        int noTake = helper(idx+1,nums,prev,dp);
-        int take = 0;
-        if(prev == -1 || nums[idx] > nums[prev])
-        {
-            take = 1 + helper(idx+1,nums,idx,dp);
+            return 0;
         }
 
-        return dp[idx][prev+1] = max(noTake, take);
+        if (dp[idx][prev + 1] != -1)
+            return dp[idx][prev + 1];
 
+        int noTake = solve(idx + 1, prev, nums, dp);
+        int take = 0;
 
+        if (prev == -1 || nums[idx] > nums[prev]) {
+            take = 1  + solve(idx + 1, idx, nums, dp);
+        }
+
+        return dp[idx][prev + 1] = max(noTake, take);
     }
     int lengthOfLIS(vector<int>& nums) {
+
         int n = nums.size();
 
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
 
-        return helper(0,nums,-1,dp);
-        
+        return solve(0, -1, nums, dp);
     }
 };
