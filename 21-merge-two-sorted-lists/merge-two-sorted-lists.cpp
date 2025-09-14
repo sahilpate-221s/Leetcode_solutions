d9e10dda7c71db84bcafb606a7853d2aa026b9d2
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-09-15 05:26:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (29.35%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -10,30 +10,23 @@
  */
 class Solution {
 public:
-    ListNode* mergeLists(ListNode* l1, ListNode* l2)
-    {
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+
         if(!l1)return l2;
         if(!l2)return l1;
 
-        if(l1->val <= l2->val)
+        if(l1->val < l2->val)
         {
-            l1->next = mergeLists(l1->next, l2);
+            l1->next = mergeTwoLists(l1->next,l2);
             return l1;
         }
-        if(l2->val < l1->val)
+        else
         {
-            l2->next = mergeLists(l1,l2->next);
+            l2->next =mergeTwoLists(l1,l2->next);
             return l2;
         }
 
         return l1;
-    }
-
-
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(!list1)return list2;
-        if(!list2)return list1;
-
-        return mergeLists(list1, list2);
+        
     }
 };
