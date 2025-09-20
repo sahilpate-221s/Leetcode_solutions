45b1558dc0840f7928ee7149d94ce84867aa3766
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-09-20 16:50:47 +0530

    Time: 25 ms (35.78%) | Memory: 23.4 MB (90.21%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,18 +11,21 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (!root) return NULL;
+        if(!root)return NULL;
 
-        // If both p and q are smaller than root, LCA lies in left subtree
-        if (p->val < root->val && q->val < root->val) {
-            return lowestCommonAncestor(root->left, p, q);
+        // if both p and q are less than root then lca found in left side
+        if(p-> val < root->val && q->val < root->val)
+        {
+            return lowestCommonAncestor(root->left, p,q);
         }
-        // If both p and q are greater than root, LCA lies in right subtree
-        else if (p->val > root->val && q->val > root->val) {
-            return lowestCommonAncestor(root->right, p, q);
+
+        // if both p and q are greater than root lca lies in right side
+        if(p->val > root->val && q->val > root->val)
+        {
+            return lowestCommonAncestor(root->right, p,q);
         }
-        // Otherwise, root is the LCA
+
         return root;
-
+        
     }
 };
