ebdd9d8c32582aab9bdcf783186ed5a8a24c66fd
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-09-20 17:02:40 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (75.21%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -12,20 +12,20 @@
  */
 class Solution {
 public:
-    bool validate(TreeNode* root, long long mini, long long maxi) {
+    bool helper(TreeNode* root, long long mini, long long maxi) {
         if (!root)
             return true;
 
         if (root->val <= mini || root->val >= maxi)
             return false;
 
-        return validate(root->left, mini, root->val) &&
-               validate(root->right, root->val, maxi);
+        return helper(root->left, mini, root->val) &&
+               helper(root->right, root->val, maxi);
     }
     bool isValidBST(TreeNode* root) {
         if (!root)
             return true;
 
-        return validate(root, LLONG_MIN, LLONG_MAX);
+        return helper(root, LLONG_MIN, LLONG_MAX);
     }
 };
