822205d9589853af3de02ba16b736e88a1179aa4
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-09-25 07:02:29 +0530

    Time: 11 ms (50.90%) | Memory: 11.4 MB (83.64%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,44 +1,51 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+
         int n = s.length();
+
         int remaining = t.length();
 
         if (remaining > n)
             return "";
 
         unordered_map<char, int> umap;
-
         for (auto it : t) {
             umap[it]++;
         }
 
+        int mini = INT_MAX;
         int startIdx = 0;
-        int miniLen = INT_MAX;
         int j = 0;
+
+        string str = "";
+
         for (int i = 0; i < n; i++) {
-            if (umap[s[i]] > 0) {
+
+            umap[s[i]]--;
+            if (umap[s[i]] >= 0) {
                 remaining--;
             }
 
-            while (remaining <= 0) {
-                if (remaining == 0) {
-                    if (i - j + 1 < miniLen) {
-                        startIdx = j;
-                        miniLen =  i - j + 1;
-                    }
+            while (remaining == 0) {
+
+                if (i - j + 1 < mini) {
+                    startIdx = j;
+                    mini = i - j + 1;
                 }
 
                 umap[s[j]]++;
-                if (umap[s[j]] > 0)
+                if (umap[s[j]] > 0) {
                     remaining++;
+                }
+
                 j++;
             }
-            umap[s[i]]--;
         }
 
-        if (miniLen == INT_MAX)
+        if (mini == INT_MAX)
             return "";
-        return s.substr(startIdx, miniLen);
+        str = s.substr(startIdx, mini);
+        return str;
     }
 };
