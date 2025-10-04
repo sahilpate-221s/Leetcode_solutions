2138eaed23a4a64c964d47dc6f3e691061aeddaa
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-10-04 18:45:52 +0530

    Time: 100 ms (73.13%) | Memory: 111.1 MB (23.24%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
+    bool helper(vector<int>& nums, int idx, int target,
+                vector<vector<int>>& dp) {
+        // base case
+        if (target == 0)
+            return true;
+        if (idx == 0)
+            return nums[idx] == target;
 
-    bool helper(vector<int>& nums, int idx, int target, vector<vector<int>>& dp)
-    {
-        //base case
-        if(target==0)return true;
-        if(idx==0)return nums[idx] == target;
+        if (dp[idx][target] != -1)
+            return dp[idx][target];
 
-
-        if(dp[idx][target] != -1)return dp[idx][target];
-
-        bool noTake = helper(nums, idx-1, target, dp);
+        bool noTake = helper(nums, idx - 1, target, dp);
         bool take = false;
 
-        if(nums[idx] <= target)
-        {
-            take = helper(nums, idx-1, target-nums[idx], dp);
+        if (nums[idx] <= target) {
+            take = helper(nums, idx - 1, target - nums[idx], dp);
         }
 
         return dp[idx][target] = noTake || take;
@@ -23,15 +23,43 @@ public:
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
 
-        int sum = accumulate(nums.begin(),nums.end(),0);
+        int sum = accumulate(nums.begin(), nums.end(), 0);
 
-        if(sum % 2 != 0)return false;
+        if (sum % 2 != 0)
+            return false;
 
-        int target = sum/2;
+        int target = sum / 2;
 
-        vector<vector<int>>dp(n+1, vector<int>(sum+1,-1));
+        vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
 
-        return helper(nums, n-1,target,dp);
-        
+        // return helper(nums, n - 1, target, dp);
+
+        for(int i=0;i<n;i++)
+        {
+            dp[i][0] = true;
+        }
+
+        if(nums[0] <= target)
+        {
+            dp[0][nums[0]]=true;
+        }
+
+
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<=target;j++)
+            {
+                int noTake = dp[i-1][j];
+                int take = 0;
+                if(nums[i] <= j)
+                {
+                    take = dp[i-1][j-nums[i]];
+                }
+
+                dp[i][j] = noTake || take;
+            }
+        }
+        return dp[n-1][target];
+
     }
 };
