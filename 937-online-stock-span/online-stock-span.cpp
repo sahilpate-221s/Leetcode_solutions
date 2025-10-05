4d565b466568c15de1910588fe28280f1cede815
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-10-05 18:37:09 +0530

    Time: 24 ms (82.60%) | Memory: 91.9 MB (89.63%) - LeetSync

--- 937-online-stock-span/online-stock-span.cpp
+++ 937-online-stock-span/online-stock-span.cpp
@@ -1,20 +1,18 @@
 class StockSpanner {
 public:
-    stack<pair<int,int>> st; //pair  of {price,span};
-    StockSpanner() {
-        
-    }
-    
+    stack<pair<int, int>> st;
+    StockSpanner() {}
+
     int next(int price) {
-        int span = 1; // currently space of price is 1 which is itself
-        while(!st.empty() and st.top().first <= price)
-        {
-            span = span+=st.top().second;
+
+        int span = 1;
+
+        while (!st.empty() && st.top().first <= price) {
+            span += st.top().second;
             st.pop();
         }
-        st.push({price,span});
-        return span;
-        
+        st.push({price, span});
+        return st.top().second;
     }
 };
 
