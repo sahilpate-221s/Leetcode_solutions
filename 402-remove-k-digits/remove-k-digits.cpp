e52e5f80fbe05833c956cb5c0c2f61e8ce8c01be
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-10-09 02:11:45 +0530

    Time: 7 ms (25.53%) | Memory: 11.8 MB (22.76%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -2,33 +2,50 @@ class Solution {
 public:
     string removeKdigits(string num, int k) {
         
-        string result = ""; //it will act like a stack
-        int n = num.length();
-        
-        for(int i = 0; i < n; i++) {
-            
-            while(result.length() > 0 && result.back() > num[i] && k > 0) {
-                result.pop_back();
+        int n= num.size();
+        stack<char>st;
+
+        for(auto c: num)
+        {
+            // Jab tak top element bada hai aur hum remove kar sakte hain
+            while(!st.empty() && k > 0 && st.top() > c)
+            {
+                st.pop();
                 k--;
             }
-            
-            if(result.length() > 0 || num[i] != '0') {
-                result.push_back(num[i]); //to avoid the case when we have preceeding zeros
-            }
-            
+            st.push(c);
         }
-        
-        
-        while(result.length() > 0 && k > 0) {
-            result.pop_back();
+
+        //agar abhi bhi koi digits bachi ho remove karne ko
+        while(k > 0 && !st.empty())
+        {
+            st.pop();
             k--;
         }
 
-        if(result == "") {
-            return "0";
+        //stack se number banao reverse banega
+        string res = "";
+
+        while(!st.empty())
+        {
+            res+= st.top();
+            st.pop();
         }
-        
-        return result;
-        
+        reverse(begin(res),end(res));
+
+
+        int i=0;
+        //leading zeros hatana hai
+        while(i < res.size() && res[i] == '0') i++;
+
+        res = res.substr(i);
+
+
+        if(res == "")return "0";
+
+        return res;
+
+
+
     }
 };
