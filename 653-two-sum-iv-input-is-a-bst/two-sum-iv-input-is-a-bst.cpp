9269657bc5ca80d720b98bffe3802c67d3935422
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-10-12 17:38:58 +0530

    Time: 6 ms (31.48%) | Memory: 38.9 MB (26.90%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -6,27 +6,31 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    unordered_set<int>umap;
+    unordered_set<int> umap;
 
-    bool dfs(TreeNode* root, int k)
-    {
-        if(!root)return false;
+    bool dfs(TreeNode* root, int k) {
+        if (!root)
+            return false;
 
-        if(umap.find(k-root->val) != umap.end())return true;
-        umap.insert(root->val);
+        int val = root->val;
+
+        if (umap.find(k - val) != umap.end())
+            return true;
+        umap.insert(val);
 
         return dfs(root->left, k) || dfs(root->right, k);
-        
     }
+
     bool findTarget(TreeNode* root, int k) {
-        if(!root)return false;
+        if (!root)
+            return false;
 
         return dfs(root, k);
-        
     }
 };
