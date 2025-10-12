be09e997fbd85a169b04e9f7d556e816025b685e
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-10-12 22:10:56 +0530

    Time: 1 ms (52.50%) | Memory: 14.3 MB (22.89%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
-    vector<vector<int>>answer;
+    // set<vector<int>> st;
+    vector<vector<int>> answer;
 
-    void solve(vector<int>& nums, int idx, vector<int>temp, int target)
-    {
-        if(target == 0)
-        {
+    void helper(vector<int>& nums, int target, int idx, vector<int>& temp) {
+        // base case
+
+        if (target == 0) {
             answer.push_back(temp);
             return;
         }
 
-        for(int i=idx;i<nums.size();i++)
-        {
-            if(i> idx && nums[i]==nums[i-1])continue;
-            if(nums[i] > target)break;
-            temp.push_back(nums[i]);
+        for (int i = idx; i < nums.size(); i++) {
+            if (i > idx && nums[i] == nums[i - 1])
+                continue;
 
-            solve(nums, i+1,temp,target-nums[i]);
+            if (nums[i] > target)
+                break;
+
+            temp.push_back(nums[i]);
+            helper(nums, target - nums[i], i + 1, temp);
             temp.pop_back();
         }
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
 
+        sort(candidates.begin(), candidates.end());
+        vector<int> temp;
+        helper(candidates, target, 0, temp);
 
-        vector<int>temp;
-        sort(begin(candidates),end(candidates));
-
-        solve(candidates,0,temp,target);
+        // for (auto it : st) {
+        //     answer.push_back(it);
+        // }
         return answer;
-        
     }
 };
