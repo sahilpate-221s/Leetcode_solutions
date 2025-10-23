d7e62069488a93570a973beed45a95dea69f8058
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-10-23 17:37:34 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (45.12%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     bool check(vector<int>& nums) {
         int count = 0;
-        for(int i=1;i<nums.size();i++)
+
+            int n = nums.size();
+        for(int i=1;i<n;i++)
         {
-            if(nums[i-1] > nums[i])
-            {
-                count++;
-            }
+            if(nums[i] < nums[i-1])count++;
         }
+        if(nums[n-1] > nums[0])count++; // it makes sures that the rotation doesnt affect the sorting
 
-        if(nums[nums.size()-1] > nums[0])count++;
-
-        return count<=1;
+        return count <= 1 ? true:false;
         
     }
 };
