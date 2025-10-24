53e77a8bd7a4a8fef40f3803e0be94d3ba8d9f40
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-10-24 23:36:59 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (67.65%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-
         int n = nums.size();
 
         int i = n - 2;
-        // step1 : finding (ith) element which is smaller than (i+1)
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
+        while (i >= 0 && nums[i] >= nums[i + 1])
             i--;
-        }
 
-        // finding the successor means finding element  from the last which is
-        // greater than (ith element)
         if (i >= 0) {
             int j = n - 1;
-            while (nums[j] <= nums[i]) {
+
+            while (j > i && nums[j] <= nums[i])
                 j--;
-            }
-            swap(nums[j], nums[i]);
+            
+            swap(nums[i], nums[j]);
         }
-        // return the suffix part only not the whole array
-        reverse(nums.begin() + i + 1, nums.end());
+
+        reverse(nums.begin() + i+1, nums.end());
     }
 };
