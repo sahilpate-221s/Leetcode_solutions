764997355e75b57fdd398785294da907d4c2ac36
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-10-25 17:52:08 +0530

    Time: 11 ms (26.21%) | Memory: 12.2 MB (68.20%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> umap;
+        unordered_map<char,int>umap;
         umap['I'] = 1;
         umap['V'] = 5;
         umap['X'] = 10;
@@ -9,10 +9,11 @@ public:
         umap['C'] = 100;
         umap['D'] = 500;
         umap['M'] = 1000;
-        int n = s.length();
-        int result = 0;
         
 
+        int n =  s.length();
+
+        int result = 0;
         for(int i=0;i<n-1;i++)
         {
             if(umap[s[i+1]] > umap[s[i]])
@@ -24,10 +25,8 @@ public:
                 result+= umap[s[i]];
             }
         }
+        result+=umap[s[n-1]];
 
-        result+= umap[s[n-1]];
         return result;
-
-
     }
 };
