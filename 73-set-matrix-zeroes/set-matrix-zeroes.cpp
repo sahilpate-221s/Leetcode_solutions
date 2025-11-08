3097f5575cdb7a85d60ae361ae2dc198024d46c7
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-11-08 06:03:38 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (11.13%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+        
         int n = matrix.size();
         int m = matrix[0].size();
 
-        int count = 1;
+        vector<int>rowVector(m,0);
+        vector<int>colVector(n,0);
 
-        for (int i = 0; i < n; i++) {
-            if (matrix[i][0] == 0) {
-                count = 0;
-            }
-            for (int j = 1; j < m; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j =0;j<m;j++)
+            {
+                if(matrix[i][j] == 0)
+                {
+                    rowVector[j] = 1;
+                    colVector[i] = 1;
                 }
             }
         }
 
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(rowVector[j] == 1 || colVector[i] == 1)
+                {
                     matrix[i][j] = 0;
                 }
-            }
-        }
-
-        // change the row
-        if (matrix[0][0] == 0) {
-            for (int j = 0; j < m; j++) {
-                matrix[0][j] = 0;
-            }
-        }
-
-        if (count == 0) {
-            for (int i = 0; i < n; i++) {
-                matrix[i][0] = 0;
             }
         }
     }
