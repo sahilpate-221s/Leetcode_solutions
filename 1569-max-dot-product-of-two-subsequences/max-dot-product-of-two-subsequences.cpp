386bc512ccb5b6bb2d4fc00869ad1007676d6587
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2026-01-09 01:16:23 +0530

    Time: 9 ms (91.02%) | Memory: 17.8 MB (55.10%) - LeetSync

---
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+        
+        vector<vector<int>> dp(n, vector<int>(m));
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                
+                // Calculate the product of the current pair
+                int product = nums1[i] * nums2[j];
+                
+                // 1. Start fresh with the current pair
+                // (Initialize with 'product' so we don't default to 0 incorrectly if answer is negative)
+                dp[i][j] = product;
+
+                // 2. Extend the previous best sequence (Add current pair to diagonal)
+                if (i > 0 && j > 0) {
+                    dp[i][j] = max(dp[i][j], dp[i-1][j-1] + product);
+                }
+
+                // 3. Skip the current number in nums1 (Inherit from top)
+                if (i > 0) {
+                    dp[i][j] = max(dp[i][j], dp[i-1][j]);
+                }
+
+                // 4. Skip the current number in nums2 (Inherit from left)
+                if (j > 0) {
+                    dp[i][j] = max(dp[i][j], dp[i][j-1]);
+                }
+            }
+        }
+
+        // The bottom-right cell contains the result for the full arrays
+        return dp[n-1][m-1];
+    }
+};
