f12e4469241d8df152b3bc2d4193c198fb6f088d
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2026-01-29 18:25:53 +0530

    Time: 4 ms (21.23%) | Memory: 40.2 MB (58.36%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+        int size = nums.size();
+
         int n = nums.size();
+        vector<int> answer(n, 1);
 
-        // prefix sum approach
-        vector<int> leftMultipleSum(n, 1);
-        vector<int> rightMultipleSum(n, 1);
-
-        vector<int> answer(n);
-
+        // Prefix products
         for (int i = 1; i < n; i++) {
-            // left = left * nums[i-1];
-            leftMultipleSum[i] = leftMultipleSum[i - 1] * nums[i - 1];
+            answer[i] = answer[i - 1] * nums[i - 1];
         }
 
-        for (int i = n - 2; i >= 0; i--) {
-            rightMultipleSum[i] = rightMultipleSum[i + 1] * nums[i + 1];
+        // Suffix product stored in a single variable
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] *= suffix;
+            suffix *= nums[i];
         }
 
-        for (int i = 0; i < n; i++) {
-            answer[i] = leftMultipleSum[i] * rightMultipleSum[i];
-        }
         return answer;
     }
 };
