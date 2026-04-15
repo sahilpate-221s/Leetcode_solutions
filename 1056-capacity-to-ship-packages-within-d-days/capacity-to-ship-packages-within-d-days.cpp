4edd8cf57784d9c25057f0d40cb60506e199887c
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2026-04-15 11:42:27 +0530

    Time: 20 ms (6.77%) | Memory: 35.1 MB (19.58%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
-    bool isPossible(vector<int>& nums, int mid, int maxdays) {
-        int load = 0;
+    int isPossible(vector<int>& nums, int capacity) {
         int days = 1;
+        int curr = 0;
 
-        for (int i = 0; i < nums.size(); i++) {
+        for (auto it : nums) {
+            if (it > capacity)
+                return INT_MAX;
 
-            if (load+ nums[i] <= mid)
-            {
-                load=load+nums[i];
-            }
-            else
-            {
-
+            if (curr + it <= capacity) {
+                curr += it;
+            } else {
+                curr = it;
                 days++;
-                load = nums[i];
             }
         }
-
-        return days <= maxdays;
+        return days;
     }
     int shipWithinDays(vector<int>& weights, int days) {
-
-        int left = *max_element(weights.begin(), weights.end());
+        int size = weights.size();
+        int left = 0;
         int right = accumulate(weights.begin(), weights.end(), 0);
 
+        int ans = -1;
         while (left <= right) {
-            long long mid = (left + right) / 2;
-
-            if (isPossible(weights, mid, days)) {
+            int mid = left + (right - left) / 2;
+            if (isPossible(weights, mid) <= days) {
+                ans = mid;
                 right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
-        return left;
+        return ans;
     }
 };
